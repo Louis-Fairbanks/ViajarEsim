9c73d4212aadebb28d97272ebd70f0d63e8a21e1
Louis-Fairbanks <Louis-Fairbanks>
2024-08-23 12:03:31 -0300

    arreglar imagen de fondo en seccion heroe

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,8 @@ export default function Home() {
     <div className="h-screen flex flex-col">
       <TopBar />
       <Header />
-      <div className='flex items-center flex-grow overflow-x-hidden'>
+      <div className='flex items-center flex-grow overflow-x-hidden
+      bg-right bg-[length:40%] bg-no-repeat bg-[url("/media/celular-con-mano-final.png")]'>
         <div className='text-center lg:text-left flex flex-col w-full lg:w-1/2 px-24 sm:px-64 justify-center items-center lg:items-start
         bg-no-repeat bg-contain bg-right bg-[url("/media/fondo-heroe-movil.svg")] h-full lg:bg-none'>
           <p className="text-text-faded">eSim Internacional</p>
@@ -29,14 +30,14 @@ export default function Home() {
           <p>¿Preparando tu próximo viaje? Con ViajareSIM, disfruta de internet de alta velocidad sin limites y olvidate de las costosas tarifas de roaming.</p>
           <Search extraClasses="w-full sm:w-2/3 lg:w-full"/>
         </div>
-        <div className="hidden relative lg:block lg:translate-x-128 xl:translate-x-0 w-1/2 h-full">
+        {/* <div className="hidden relative lg:block lg:translate-x-128 xl:translate-x-0 w-1/2 h-full">
         <Image loading="eager"
           priority
-            src='/media/hero-section.png'
+            src='/media/celular-con-mano-final.png'
             alt='mano con celular'
             fill={true}
           />
-        </div>
+        </div> */}
       </div>
     </div>
     <Advantages />

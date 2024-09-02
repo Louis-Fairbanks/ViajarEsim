9b47052369a983978dc28afe9acc7db4636cdbc6
Louis-Fairbanks <Louis-Fairbanks>
2024-09-02 02:25:19 -0300

    sacar useparams

--- app/pago-exitoso/page.tsx
+++ app/pago-exitoso/page.tsx
@@ -1,15 +1,15 @@
 'use client'
 import React from 'react'
-import { useParams } from 'next/navigation';
+// import { useParams } from 'next/navigation';
 
 const page = () => {
 
-    const params = useParams();
+    // const params = useParams();
 
   return (
     <div>
         <h1>Pago exitoso</h1>
-        <p>Gracias por tu compra {params[0]}</p>
+        <p>Gracias por tu compra</p>
     </div>
   )
 }

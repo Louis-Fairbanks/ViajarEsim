aed461078e36bb65eef7eea795043b657ea53c4b
Louis-Fairbanks <Louis-Fairbanks>
2024-09-09 23:14:26 -0300

    arreglar seccion de como instalar una seccion con imagen de iphone

--- app/como-instalar-una-esim/MovingStepsSection.tsx
+++ app/como-instalar-una-esim/MovingStepsSection.tsx
@@ -1,41 +1,23 @@
-'use client'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import SectionHeader from '../components/ReusableComponents/SectionHeader'
 import InstallationSteps from './InstallationSteps'
 import SwitchingPhoneContainer from './SwitchingPhoneContainer'
-import { useInstallation } from './InstallationProvider'
 
 type Props = {
     sectionTitle: string;
     sectionHeader: string;
-    isInstallOrActivateSteps: string;
+    isInstallOrActivateSteps: 'install' | 'activate';
 }
 
-const MovingStepsSection = ({ sectionTitle, sectionHeader, isInstallOrActivateSteps} : Props) => {
-    const [currentStep, setCurrentStep] = useState<number>(1)
-    const {selectedDevice, installationType} = useInstallation()
-
-    useEffect(() => {
-        console.log('MovingStepsSection re-rendered');
-        console.log('Selected Device:', selectedDevice);
-        console.log('Installation Type:', installationType);
-    }, [selectedDevice, installationType]);
-
+const MovingStepsSection: React.FC<Props> = ({ sectionTitle, sectionHeader, isInstallOrActivateSteps }) => {
     return (
         <div className='flex flex-col space-y-48 p-24 sm:p-64'>
             <SectionHeader title={sectionTitle} header={sectionHeader} />
             <div className='flex space-x-48'>      
                 <InstallationSteps 
-                    currentStep={currentStep} 
-                    selectedDevice={selectedDevice} 
-                    installationType={installationType} 
                     isInstallOrActivateSteps={isInstallOrActivateSteps}
                 />
                 <SwitchingPhoneContainer 
-                    currentStep={currentStep} 
-                    setCurrentStep={setCurrentStep}
-                    selectedDevice={selectedDevice} 
-                    installationType={installationType} 
                     isInstallOrActivateSteps={isInstallOrActivateSteps} 
                 />
             </div>

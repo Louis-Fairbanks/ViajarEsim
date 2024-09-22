57a1c6a18742ecbea224eb33dbe70c9ac584a6da
Louis-Fairbanks <Louis-Fairbanks>
2024-09-22 16:13:29 -0300

    correr chat widget para arriba

--- app/components/ReusableComponents/ChatScript.tsx
+++ app/components/ReusableComponents/ChatScript.tsx
@@ -3,11 +3,18 @@ import Script from 'next/script'
 
 const ChatScript = () => {
   return (
+    <>
+      <style>{`
+    #_7b220 ._98fce{
+      bottom: 30px !important;
+    }
+  `}</style>
     <Script
         id='respondio__widget'
         strategy='afterInteractive'
         src='https://cdn.respond.io/webchat/widget/widget.js?cId=85a38a28525271bf84fb1e834f4015c'
     />
+    </>
   )
 }
 
